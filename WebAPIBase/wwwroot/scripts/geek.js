4e20112732e2b16c5e18c75d52e14cc3a5e0ae65
Janka Balogh <jankuci2000@gmail.com>
2023-11-27 14:12:51 +0100

    geek is frissítve

---
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -0,0 +1,25 @@
+﻿
+window.onload = function letöltés() {
+    fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+}
+
+function letöltésBefejeződött(d) {
+
+    let k = document.getElementById("kerdes")
+
+    for (var i = 0; i < d.length; i++) {
+
+        console.log(d[i].question)
+
+        let kérdés = document.createElement("div")
+        kérdés.innerHTML = d[i].question
+        k.appendChild(kérdés)
+        
+
+    }
+
+
+}
